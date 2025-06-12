f93bc0aab2ae6a458ced3504fc1ef9cab39e8276
beandotmp3 <beandotmp3@gmail.com>
2025-06-12 11:21:21 -0600

    started remaking input system added support for modifiers using Press(), formerly PressKey() added support for "special" keys

--- Main.cs
+++ Main.cs
@@ -9,19 +9,37 @@ public class Input
  private const int EVENT_KEYDOWN = 0x0000;
  private const int EVENT_KEYUP = 0x0002;
 
- public static void PressKey(string key)
+ public static void Type(string input)
  {
+  
+ }
+
+ public static void Press(string key)
+ {
+  
+ }
+ public static void Press(string modifier, string key)
+ {
+  modifier = modifier.ToUpper();
   key = key.ToUpper();
-  byte vCode = KeyMap.vKeys[key];
-  keybd_event(vCode, 0, EVENT_KEYDOWN, UIntPtr.Zero);
-  keybd_event(vCode, 0, EVENT_KEYUP, UIntPtr.Zero);
+  byte vM = KeyMap.vMods[modifier];
+  byte vK = KeyMap.vKeys[key];
+  
+  keybd_event(vM, 0, EVENT_KEYDOWN, UIntPtr.Zero);
+  keybd_event(vK, 0, EVENT_KEYDOWN, UIntPtr.Zero);
+  keybd_event(vK, 0, EVENT_KEYUP, UIntPtr.Zero);
+  keybd_event(vM, 0, EVENT_KEYUP, UIntPtr.Zero);
+ }
+ public static void Press(string modifier1, string modifier2, string key)
+ {
+
  }
 
  public static void Space()
  {
   keybd_event(0x20, 0, EVENT_KEYDOWN, UIntPtr.Zero);
   keybd_event(0x20, 0, EVENT_KEYUP, UIntPtr.Zero);
-  Thread.Sleep(10);
+  Thread.Sleep(15);
  }
 }
 
@@ -34,15 +52,16 @@ class KeyMap
   {"V", 0x56}, {"W", 0x57}, {"X", 0x58}, {"Y", 0x59}, {"Z", 0x5A},
 
   {"0", 0x30}, {"1", 0x31}, {"2", 0x32}, {"3", 0x33}, {"4", 0x34}, {"5", 0x35}, {"6", 0x36}, {"7", 0x37}, {"8", 0x38}, {"9", 0x39},
-  
-  {"!", 0x31}, {"@", 0x32}, {"#", 0x33}, {"$", 0x34}, {"%", 0x35}, {"^", 0x36}, {"&", 0x37}, {"*", 0x38}, {"(", 0x39}, {")", 0x30},
 
   {"BACKSPACE", 0x08}, {"DELETE", 0x2E}, {"TAB", 0x09}, {"ENTER", 0x0D}
  };
  public static readonly Dictionary<string, byte> vSpec = new() {
-  
+  {"F1", 0x70}, {"F2", 0x71}, {"F3", 0x72}, {"F4", 0x73}, {"F5", 0x74}, {"F6", 0x75}, 
+  {"F7", 0x76}, {"F8", 0x77}, {"F9", 0x78}, {"F10", 0x79}, {"F11", 0x7A}, {"F12", 0x7B},
+
+  {"ESCAPE", 0x1B}, {"HOME", 0x24}, {"END", 0x23}, {"INSERT", 0x2D}, {"PAGEUP", 0x21}, {"PAGEDOWN", 0x22}
  };
  public static readonly Dictionary<string, byte> vMods = new() {
-  {"SHIFT", 0x10}, {"CONTROL", 0x11}, {"ALT", 0x12}
+  {"SHIFT", 0x10}, {"CONTROL", 0x11}, {"ALT", 0x12}, {"WIN", 0x5B}
  };
 }
