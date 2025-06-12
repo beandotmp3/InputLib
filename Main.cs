f3df4d444f327d83dcc900737e79fd3e87ce23cd
beandotmp3 <beandotmp3@gmail.com>
2025-06-12 09:10:54 -0600

    added numbers as valid inputs

--- Main.cs
+++ Main.cs
@@ -12,7 +12,7 @@ public class Input
  public static void PressKey(string key)
  {
   key = key.ToUpper();
-  byte vCode = KeyMap.vKeyCodes[key];
+  byte vCode = KeyMap.vKeys[key];
   keybd_event(vCode, 0, EVENT_KEYDOWN, UIntPtr.Zero);
   keybd_event(vCode, 0, EVENT_KEYUP, UIntPtr.Zero);
  }
@@ -27,13 +27,15 @@ public class Input
 
 class KeyMap
 {
- public static readonly Dictionary<string, byte> vKeyCodes = new() { 
+ public static readonly Dictionary<string, byte> vKeys = new() { 
   {"A", 0x41}, {"B", 0x42}, {"C", 0x43}, {"D", 0x44}, {"E", 0x45}, {"F", 0x46}, {"G", 0x47},
   {"H", 0x48}, {"I", 0x49}, {"J", 0x4A}, {"K", 0x4B}, {"L", 0x4C}, {"M", 0x4D}, {"N", 0x4E},
   {"O", 0x4F}, {"P", 0x50}, {"Q", 0x51}, {"R", 0x52}, {"S", 0x53}, {"T", 0x54}, {"U", 0x55},
-  {"V", 0x56}, {"W", 0x57}, {"X", 0x58}, {"Y", 0x59}, {"Z", 0x5A}
+  {"V", 0x56}, {"W", 0x57}, {"X", 0x58}, {"Y", 0x59}, {"Z", 0x5A},
+
+  {"0", 0x30}, {"1", 0x31}, {"2", 0x32}, {"3", 0x33}, {"4", 0x34}, {"5", 0x35}, {"6", 0x36}, {"7", 0x37}, {"8", 0x38}, {"9", 0x39}
  };
- public static readonly Dictionary<string, byte> vModCodes = new() {
+ public static readonly Dictionary<string, byte> vMods = new() {
   {"SHIFT", 0x10}, {"CONTROL", 0x11}, {"ALT", 0x12}
  };
 }
